41a5da0227e8979713da635aa41ce0bc7caa76ce
jaebooker <jaesonbooker@gmail.com>
2018-12-04 22:13:31 -0800

    adding more to swiftchain

--- Sources/App/Models/Models.swift
+++ Sources/App/Models/Models.swift
@@ -8,6 +8,10 @@
 import Foundation
 import Vapor
 
+let genesisBlock = Block()
+let blockchain = Blockchain(genesisBlock: genesisBlock)
+
+
 protocol SmartContract {
     func apply(transaction: Transaction)
 }
@@ -45,7 +49,7 @@ class Transaction: Codable {
         self.transactionType = transactionType
     }
 }
-class Block {
+class Block: Codable {
     var index: Int = 0
     var previousHash: String = ""
     var hash: String!
@@ -67,8 +71,61 @@ class Block {
 }
 class Blockchain {
     private (set) var blocks: [Block] = [Block]()
+    init(genesisBlock: Block) {
+        addBlock(genesisBlock)
+    }
+    func addBlock(_ block: Block) {
+        if self.blocks.isEmpty {
+            block.previousHash = "000000000000000"
+            block.hash = generateHash(for: block)
+        }
+        self.blocks.append(block)
+    }
+    func getNextBlock(transactions: [Transaction]) -> Block {
+        let block = Block()
+        transactions.forEach { transaction in
+            block.addTransaction(transaction: transaction)
+            }
+        let previousBlock = getPreviousBlock()
+        block.index = self.blocks.count
+        block.previousHash = previousBlock.hash
+        block.hash = generateHash(for: block)
+        return block
+    }
+    private func getPreviousBlock() -> Block {
+        return self.blocks[self.blocks.count - 1]
+    }
+    func generateHash(for block: Block) -> String {
+        var hash = block.key.sha1Hash()
+        
+        while(!hash.hasPrefix("00")) {
+            block.nonce += 1
+            hash = block.key.sha1Hash()
+            print(hash)
+        }
+            
+        return hash
+    }
 }
 let transaction = Transaction(from: "Peter", to: "Paul", amount: 2000, transactionType: .domestic)
+let block = blockchain.getNextBlock(transactions: [transaction])
 let block1 = Block()
 //block1.addTransaction(transaction: transaction)
 //block1.key
+extension String {
+    func sha1Hash() -> String {
+        let task = Process()
+        task.launchPath = "/usr/bin/shasum"
+        task.arguments = []
+        let inputPipe = Pipe()
+        inputPipe.fileHandleForWriting.write(self.data(using: String.Encoding.utf8)!)
+        inputPipe.fileHandleForWriting.closeFile()
+        let outputPipe = Pipe()
+        task.standardOutput = outputPipe
+        task.standardInput = inputPipe
+        task.launch()
+        let data = outputPipe.fileHandleForReading.readDataToEndOfFile()
+        let hash = String(data: data, encoding: String.Encoding.utf8)!
+        return hash.replacingOccurrences(of: "  -\n", with: "")
+    }
+}
