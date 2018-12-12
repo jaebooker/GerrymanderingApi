ccec66e8e456c44950415e1672e3e4f6d172a52c
jaebooker <jaesonbooker@gmail.com>
2018-12-12 14:24:31 -0800

    new

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
 
         //  Swift ORM (queries, models, relations, etc) built on SQLite 3.
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0"),
-        .package(url: "https://github.com/vapor/auth.git", from: "2.0.0"),
+        .package(url: "https://github.com/vapor/auth.git", from: "2.0.1"),
         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0")
     ],
     targets: [
